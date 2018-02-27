b5915a273e361d9248b315d7a41d60ce6f1a96e3
Diana Espitia <dp.espitia@uniandes.edu.co>
2018-02-26 22:54:18 -0500

    Commit Gremlins

---
+++ Gremlins/test/specs/gremlins-test.js
@@ -0,0 +1,52 @@
+function loadScript(callback) {
+  var s = document.createElement('script');
+  s.src = 'https://rawgithub.com/marmelab/gremlins.js/master/gremlins.min.js';
+  if (s.addEventListener) {
+    s.addEventListener('load', callback, false);
+  } else if (s.readyState) {
+    s.onreadystatechange = callback;
+  }
+  document.body.appendChild(s);
+}
+
+function unleashGremlins(ttl, callback) {
+  function stop() {
+    horde.stop();
+    callback();
+  }
+  var horde = window.gremlins.createHorde();
+  horde.seed(1234);
+
+  horde.gremlin(gremlins.species.clicker().clickTypes(['click']))
+  .gremlin(gremlins.species.formFiller().canFillElement(function(element) { return true }));
+  horde.strategy (gremlins.strategies.distribution ()
+    .delay (50)
+    .distribution ([0.4, 0.6])
+  )
+
+  horde.after(callback);
+  window.onbeforeunload = stop;
+  setTimeout(stop, ttl);
+  horde.unleash();
+}
+
+describe('Monkey testing with gremlins ', function() {
+
+  it('it should not raise any error', function() {
+    browser.url('/');
+    browser.click('button=Cerrar');
+
+    browser.timeoutsAsyncScript(60000);
+    browser.executeAsync(loadScript);
+
+    browser.timeoutsAsyncScript(60000);
+    browser.executeAsync(unleashGremlins, 50000);
+  });
+
+  afterAll(function() {
+    browser.log('browser').value.forEach(function(log) {
+      browser.logger.info(log.message.split(' ')[2]);
+    });
+  });
+
+});
